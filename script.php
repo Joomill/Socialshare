52fc655caf5ad8d9f4f70af507948fc62ea923a4
Joomill <info@joomill.nl>
2023-09-17 20:05:51 +0200

    PHP Comments

--- script.php
+++ script.php
@@ -1,8 +1,9 @@
 <?php
 /*
- *  package: Maestro Template
- *  copyright: Copyright (c) 2020. Jeroen Moolenschot | Joomill
- *  license: GNU General Public License version 3 or later
+ *  package: Joomill - Social Share
+ *  copyright: Copyright (c) 2023. Jeroen Moolenschot | Joomill
+ *  license: GNU General Public License version 2 or later
+ *  link: https://www.joomill-extensions.com
  */
 
 // No direct access.
