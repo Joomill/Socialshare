d07dd14f16978777411a498b78b3c45990c860ff
JeroenMoolenschot <info@joomill.nl>
2025-02-24 11:55:47 +0100

    V2.0.0 - Minimum Joomla version 4.0.0

--- script.php
+++ script.php
@@ -13,6 +13,63 @@ use Joomla\CMS\Factory;
 
 class plgContentsocialshareInstallerScript
 {
+	/**
+	 * Minimum Joomla version to check
+	 *
+	 * @var    string
+	 * @since  4.0.0
+	 */
+	private $minimumJoomlaVersion = '4.0';
+
+	/**
+	 * Minimum PHP version to check
+	 *
+	 * @var    string
+	 * @since  4.0.0
+	 */
+	private $minimumPHPVersion = JOOMLA_MINIMUM_PHP;
+
+	/**
+	 * Function called before extension installation/update/removal procedure commences
+	 *
+	 * @param   string            $type    The type of change (install, update or discover_install, not uninstall)
+	 * @param   InstallerAdapter  $parent  The class calling this method
+	 *
+	 * @return  boolean  True on success
+	 * @throws Exception
+	 * @since  4.0.0
+	 */
+	public function preflight($type, $parent): bool
+	{
+		if ($type !== 'uninstall')
+		{
+			// Check for the minimum PHP version before continuing
+			if (!empty($this->minimumPHPVersion) && version_compare(PHP_VERSION, $this->minimumPHPVersion, '<'))
+			{
+				Log::add(
+					Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP', $this->minimumPHPVersion),
+					Log::WARNING,
+					'jerror'
+				);
+
+				return false;
+			}
+			// Check for the minimum Joomla version before continuing
+			if (!empty($this->minimumJoomlaVersion) && version_compare(JVERSION, $this->minimumJoomlaVersion, '<'))
+			{
+				Log::add(
+					Text::sprintf('JLIB_INSTALLER_MINIMUM_JOOMLA', $this->minimumJoomlaVersion),
+					Log::WARNING,
+					'jerror'
+				);
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	public function install($parent)
 	{
 		// Enable the extension
